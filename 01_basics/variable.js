f142be08128298f6ff3da427d5f2bb48879ed1e1
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2024-02-13 13:13:15 +0000

    important note

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -4,3 +4,6 @@ const c = 12;
 d = 13;
 let e;
 console.log(a + " " + b + " " + c + " " + d + " " + e);
+/*
+avoid using var. this will creat issues in global or local scope, use let and const only
+*/
