7d3b9935e5fc06c0e48d1bf1077e42a02d9959f8
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2024-02-13 13:20:33 +0000

    learn datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,16 @@
+/*
+number  => 2 to the power 53
+bigint => beyond number/
+string => ""
+boolean => true or false; 
+null => standalone
+undefine =>
+symbol => find uniqueness
+
+
+
+Object!!
+*/
+
+console.log(typeof undefined); //gives undefine
+console.log(typeof null); //gives Object
