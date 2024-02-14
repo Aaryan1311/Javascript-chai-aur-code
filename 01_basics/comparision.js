cfb75d39577621a837139297e86b7ba850a7f828
Aaryan1311 <100665724+Aaryan1311@users.noreply.github.com>
2024-02-14 13:18:53 +0000

    understand comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// false for all other
+
+// === 
+
+console.log("2" === 2); // check datatype and value both
